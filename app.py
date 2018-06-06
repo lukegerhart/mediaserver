00a799cbb97540d835d7f94197b9d2b3efd0037b
Luke Gerhart <lukegerhart2@gmail.com>
2018-06-06 03:39:09 +0000

    recursively generate directory tree

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, Response, jsonify, render_template
+from flask import Flask, Response, render_template
 from PIL import Image
-from utils import generateDir
-import io
+from utils import generateList
+import io, json
 app = Flask(__name__)
 basedir = '/media/pi/Seagate Backup Plus Drive1/'
 @app.route('/')
 def home():
-    dir = generateDir(basedir)
-    return render_template('home.html', dir=jsonify(dir))
+    dir = generateList(basedir)
+    return render_template('index.html', dir=dir)
 def something():
     filename = 'Xiao Bai/AIMG_4202.JPG'
     im = Image.open(basedir+filename)
--- utils.py
+++ utils.py
@@ -1,7 +1,61 @@
 import os
 from collections import OrderedDict
+from itertools import chain
 def generateDir(basedir):
     dir = OrderedDict()
     for root, dirs, files in os.walk(basedir):
         dir[root] = {'rel':root[len(basedir):], 'children':dirs, 'files':files}
     return dir
+
+class Folder:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+        self.files = []
+    def __repr__(self):
+        return self.name
+    def add_child(self, child):
+        self.children.append(child)
+    
+    def add_file(self, file):
+        self.files.append(file)
+        
+    def add_children(self, children):
+        self.children.extend(children)
+    
+    def add_files(self, files):
+        self.files.extend(files)
+    
+    def children(self):
+        return len(self.children)
+    
+    def files(self):
+        return len(self.files)
+def add(dir, rel, files, level):
+    for f in dir:
+        if isinstance(f, list):
+            continue
+        if f.name == rel[-2]:
+            folder = Folder(rel[level+1])
+            folder.add_files(files)
+            f.add_child(folder)
+            return
+    add(list(chain.from_iterable([f.children for f in dir])), rel, files, level+1)
+        
+def generateList(basedir):
+    dir = []
+    for path, children, files in os.walk(basedir):
+        if path == basedir:
+            continue
+        
+        rel = path[len(basedir):].split('/')
+        if len(rel) == 1:
+            folder = Folder(rel[0])
+            folder.add_files(files)
+            dir.append(folder)
+        else:
+            add(dir, rel, files, 0)
+    return dir
+if __name__ == '__main__':
+    for f in generateList('/media/pi/Seagate Backup Plus Drive1/'):
+        print(f, f.children, [f2.children for f2 in f.children])
